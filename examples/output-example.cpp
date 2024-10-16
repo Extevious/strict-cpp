3cbbe086004cf5d2a4eb31cb794e6d02cc12fd94
Extevious <>
2024-10-15 18:07:48 -0700

    Updated examples.

--- examples/output-example.cpp
+++ examples/output-example.cpp
@@ -6,70 +6,80 @@
 
 #include <iostream>
 
-void output(auto out) { std::cout << out << '\n'; }
+void output(const auto&... out) { (std::cout << ... << out) << '\n'; }
 
-void w_output(auto out) { std::wcout << out << '\n'; }
+void output(const auto& msg, const bool out) { std::cout << msg << (out ? "true" : "false") << '\n'; }
+
+void w_output(const auto&... out) { (std::wcout << ... << out) << '\n'; }
 
 void nl() { std::cout << '\n'; }
 
 void integral() {
    output("=== Integral ===");
 
-   strict::size_t  a = 42ULL;
+   strict::size_t  a = 40ULL;
    strict::count_t b = strict::count_t(5.0f);
 
+   output("a: ", a);
+   output("b: ", b);
+   nl();
+
    output("Arithmetic operators:");
-   output(a + b);
-   output(a - b);
-   output(a * b);
-   output(a / b);
+   output("a + b: ", a + b);
+   output("a - b: ", a - b);
+   output("a * b: ", a * b);
+   output("a / b: ", a / b);
    nl();
 
    output("Unary operators:");
-   output(+a);
-   output(-a);
+   output("+a: ", +a);
+   output("-a: ", -a);
    nl();
 
    output("Binary operators:");
-   output(~a);
-   output(a & b);
-   output(a << b);
-   output(a >> b);
-   output(a % b);
-   output(a | b);
-   output(a ^ b);
+   output("~a: ", ~a);
+   output("a & b: ", a & b);
+   output("a << b: ", a << b);
+   output("a >> b: ", a >> b);
+   output("a % b: ", a % b);
+   output("a | b: ", a | b);
+   output("a ^ b: ", a ^ b);
    nl();
 
    output("Compound assignment operators:");
-   output(a += b);
-   output(a -= b);
-   output(a *= b);
-   output(a /= b);
-   output(a &= b);
-   output(a <<= b);
-   output(a >>= b);
-   output(a %= b);
-   output(a |= b);
-   output(a ^= b);
+   output("a += b: ", a += b);
+   output("a -= b: ", a -= b);
+   output("a *= b: ", a *= b);
+   output("a /= b: ", a /= b);
+   output("a &= b: ", a &= b);
+   output("a <<= b: ", a <<= b);
+   output("a >>= b: ", a >>= b);
+   output("a %= b: ", a %= b);
+   output("a |= b: ", a |= b);
+   output("a ^= b: ", a ^= b);
    nl();
 
    output("Comparison operators:");
-   output(a == b);
-   output(a != b);
-   output(a > b);
-   output(a < b);
-   output(a >= b);
-   output(a <= b);
+   output("a == b: ", a == b);
+   output("a != b: ", a != b);
+   output("a > b: ", a > b);
+   output("a < b: ", a < b);
+   output("a >= b: ", a >= b);
+   output("a <= b: ", a <= b);
    nl();
 
    output("Increment operators:");
-   output(a++);
-   output(++a);
+   output("a++: ", a++);
+   output("a++: ", a++);
+   output("a++: ", a++);
+   output("++a: ", ++a);
+   output("++a: ", ++a);
+   output("++a: ", ++a);
    nl();
 
    output("Decrement operators:");
-   output(a--);
-   output(--a);
+   output("a--: ", a--);
+   output("--a: ", --a);
    nl();
 
    output("Casting operators:");
@@ -77,69 +87,76 @@ void integral() {
    std::size_t       s = a;
    int               i = (int)a;
    float             f = (float)a;
-   strict::count32_t c = (strict::count32_t)a;
+   strict::count32_t c = a.as<strict::count32_t>();
 
-   output(s);
-   output(i);
-   output(f);
-   output(c);
+   output("s: ", s);
+   output("i: ", i);
+   output("f: ", f);
+   output("c: ", c);
+   nl();
+
+   strict::schar_t schar0 = {};
+
+   output("Small types (char, short, etc):");
+   output("schar0: ", schar0);
+   output("schar0 + 5: ", schar0 + 5);
    nl();
 
    output("Strings:");
-   output(a);
-   output(a.to_string());
-   output(a.to_string_t());
-   output(a.to_string_n());
-   w_output(a.to_wstring());
-   w_output(a.to_wstring_t());
-   w_output(a.to_wstring_n());
+   output("a: ", a);
+   output("a.to_string(): ", a.to_string());
+   w_output("a.to_wstring(): ", a.to_wstring());
    nl();
 }
 
 void floating() {
    output("=== Floating ===");
 
-   strict::float_t a = 16.854f;
+   strict::float_t a = 10.0f;
    strict::float_t b = strict::float_t(5.4l);
 
    output("Unary operators:");
-   output(+a);
-   output(-a);
+   output("+a: ", +a);
+   output("-a: ", -a);
    nl();
 
    output("Arithmetic operators:");
-   output(a + b);
-   output(a - b);
-   output(a * b);
-   output(a / b);
-   output(a % b); // Uses std::fmod (C++23) or std::fmodf/l
+   output("a + b: ", a + b);
+   output("a - b: ", a - b);
+   output("a * b: ", a * b);
+   output("a / b: ", a / b);
+   output("a % b: ", a % b); // Uses std::fmod (C++23) or std::fmodf/l
    nl();
 
    output("Compound assignment operators:");
-   output(a += b);
-   output(a -= b);
-   output(a *= b);
-   output(a /= b);
-   output(a %= b); // Uses std::fmod (C++23) or std::fmodf/l
+   output("a += b: ", a += b);
+   output("a -= b: ", a -= b);
+   output("a *= b: ", a *= b);
+   output("a /= b: ", a /= b);
+   output("a %= b: ", a %= b); // Uses std::fmod (C++23) or std::fmodf/l
    nl();
 
    output("Comparison operators:");
-   output(a == b);
-   output(a != b);
-   output(a > b);
-   output(a < b);
-   output(a >= b);
-   output(a <= b);
+   output("a == b: ", a == b);
+   output("a != b: ", a != b);
+   output("a > b: ", a > b);
+   output("a < b: ", a < b);
+   output("a >= b: ", a >= b);
+   output("a <= b: ", a <= b);
    nl();
 
    output("Increment operators:");
-   output(a++);
-   output(++a);
+   output("a++: ", a++);
+   output("a++: ", a++);
+   output("a++: ", a++);
+   output("++a: ", ++a);
+   output("++a: ", ++a);
+   output("++a: ", ++a);
    nl();
 
    output("Decrement operators:");
-   output(a--);
-   output(--a);
+   output("a--: ", a--);
+   output("--a: ", --a);
    nl();
 
    output("Casting operators:");
@@ -147,22 +164,18 @@ void floating() {
    float             f = a;
    std::size_t       s = (std::size_t)a;
    int               i = (int)a;
-   strict::count32_t c = (strict::count32_t)a;
+   strict::count32_t c = a.as<strict::count32_t>();
 
-   output(f);
-   output(s);
-   output(i);
-   output(c);
+   output("f: ", f);
+   output("s: ", s);
+   output("i: ", i);
+   output("c: ", c);
    nl();
 
    output("Strings:");
-   output(a);
-   output(a.to_string());
-   output(a.to_string_t());
-   output(a.to_string_n());
-   w_output(a.to_wstring());
-   w_output(a.to_wstring_t());
-   w_output(a.to_wstring_n());
+   output("a: ", a);
+   output("a.to_string(): ", a.to_string());
+   w_output("a.to_wstring(): ", a.to_wstring());
    nl();
 }
 
--- examples/use-case-example.cpp
+++ examples/use-case-example.cpp
@@ -6,29 +6,51 @@
 
 #include <iostream>
 
-void output(auto... out) { (std::cout << ... << out) << '\n'; }
+void output(const auto&... out) { (std::cout << ... << out) << '\n'; }
 
+// === Addition ===
+
+// #1
 void add(strict::int32_t a, strict::int32_t b) { output("add() #1: ", a + b); }
 
+// #2
 void add(strict::int32_t a, strict::uint32_t b) { output("add() #2: ", a + b); }
 
-void add(strict::int8_t a, strict::int8_t b) { output("add() #3: ", a + b); }
+// #3
+void add(strict::int64_t a, strict::int32_t b) { output("add() #3: ", a + b); }
 
-void add(strict::int64_t a, strict::int32_t b) { output("add() #4: ", a + b); }
+// #4
+void add(strict::int8_t a, strict::int8_t b) { output("add() #4: ", a + b); }
 
+// #5
 void add(strict::int16_t a, short b) { output("add() #5: ", a + b); }
 
-void subtract(strict::size_t a, strict::size_t b) { output("subtract() #1: ", a - b); }
+// === Subtraction ===
 
-void subtract(strict::count_t a, strict::count_t b) { output("subtract() #2: ", a - b); }
+// #6
+void subtract(strict::size_t a, strict::size_t b) { output("subtract() #6: ", a - b); }
 
-void multiply(strict::float_t a, strict::float_t b) { output("multiply() #1: ", a * b); }
+// #7
+void subtract(strict::count_t a, strict::count_t b) { output("subtract() #7: ", a - b); }
 
-void multiply(strict::float_t a, strict::double_t b) { output("multiply() #2: ", a * b); }
+// #8
+void subtract(strict::count_t a, strict::size_t b) { output("subtract() #8: ", a - b); }
 
-void multiply(strict::long_double_t a, strict::double_t b) { output("multiply() #3: ", a * b); }
+// === Multiplication ===
 
-void multiply(strict::float_t a, strict::size_t b) { output("multiply() #4: ", a * b); }
+// #9
+void multiply(strict::float_t a, strict::float_t b) { output("multiply() #9: ", a * b); }
+
+// #10
+void multiply(strict::float_t a, strict::double_t b) { output("multiply() #10: ", a * b); }
+
+// #11
+void multiply(strict::long_double_t a, strict::double_t b) { output("multiply() #11: ", a * b); }
+
+// #12
+void multiply(strict::float_t a, strict::size_t b) { output("multiply() #12: ", a * b); }
+
+// === Main ===
 
 int main() {
    // strict signed int
@@ -37,69 +59,52 @@ int main() {
    // std signed int
    std::int32_t b = -34;
 
-   // Calls #1
-   add(a, b);
+   // Implicit conversions are possible for the encapsulated-type only, for example:
+   add(a, b);                             // Calls #1
+   add(a, 4);                             // Calls #1
+   add(45, 56);                           // Calls #1
+   add(a, 5U);                            // Calls #2
+   add(strict::int64_t(5.0f), 22);        // Calls #3
+   add((std::int8_t)41, (std::int8_t)65); // Calls #4
+   add((short)a, (short)12);              // Calls #5
 
-   // Calls #1
-   add(a, 4);
-
-   // Calls #2
-   add(a, 5U);
-
-   // Calls #5
-   add((short)a, (short)12);
-
-   // Calls #3
-   add((std::int8_t)41, (std::int8_t)65);
-
-   // Calls #4
-   add(strict::int64_t(5.0f), 22);
-
-   // This doesn't work because they're both unsigned ints:
+   // This doesn't work because they're both unsigned ints as there is no overload available:
    //
    //    add(5U, 9U);
    //
    // ...but this works because the constructors are being called explicitly.
-   add(strict::int32_t(5U), strict::int32_t(9U));
+   add(strict::int32_t(5U), strict::int32_t(9U)); // Calls #1
 
    // Unfortunately, there is some overlap between types when it comes to implicit
-   // conversions. Both strict::size_t and strict::count_t have the same
-   // encapsulated type (std::size_t), which results in ambiguity.
+   // conversions due to both strict::size_t and strict::count_t having the same
+   // encapsulated type (std::size_t) resulting in ambiguity between #6, #7, and #8.
    //
    //   subtract(234ULL, 78ULL);
    //
    // To get around this issue, simply convert to the desired type:
-   subtract((strict::size_t)234ULL, (strict::size_t)78ULL);
+   subtract((strict::size_t)234ULL, 78ULL);                  // Calls #6
+   subtract(234ULL, (strict::count_t)78ULL);                 // Calls #7
+   subtract((strict::count_t)234ULL, (strict::size_t)78ULL); // Calls #8
 
    // Floats are supported like you'd normally expect them to work:
-
-   // Calls #1
-   multiply(1.0f, 6.34f);
-
-   // Calls #2
-   multiply(3.5f, strict::double_t(6.34));
-
-   // Calls #3
-   multiply(2.0L, (strict::double_t)6.34);
-
-   // Calls #4
-   multiply(2.0f, strict::size_t(45));
+   multiply(1.0f, 6.34f);                  // Calls #9
+   multiply(3.5f, strict::double_t(6.34)); // Calls #10
+   multiply(2.0L, (strict::double_t)6.34); // Calls #11
+   multiply(2.0f, strict::size_t(45));     // Calls #12
 
    // Strict types can still be used on pointers like you normally would:
    constexpr const char* someMessage = "I'm a totally cool message..!";
 
-   const char* ptr = someMessage;
+   const char* character = someMessage + strict::size_t(9); // Character: a
 
-   // Character: I
-   output("Character: ", *ptr);
+   output("Character: ", *character);
 
-   ptr += strict::size_t(9);
+   character += strict::size_t(10); // Character: m
 
-   // Character: a
-   output("Character: ", *ptr);
+   output("Character: ", *character);
 
-   // Character: m
-   output("Character: ", *(ptr + strict::size_t(10)));
+   // to_string function is already provided, making stringification easy:
+   output(a.to_string());
 
    return 0;
 }
